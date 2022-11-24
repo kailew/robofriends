be5162a98884227cb4ad07f5a6654634edf2d5eb
Kai Lew <79333361+kailew@users.noreply.github.com>
2022-11-23 19:12:52 -0800

    created better performance and added PWA

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { creatLogger } from 'redux-logger';
-import { thunkMiddleware } from 'redux-thunk'
 import { createRoot } from "react-dom/client";
-import './index.css';
-import App from './containers/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
-import { searchRobots, requestRobots } from './reducers';
+
+
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+import { requestRobots, searchRobots } from './reducers'
+
+import './index.css';
+
+const logger = createLogger() 
+
+const rootReducers = combineReducers({requestRobots, searchRobots})
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+<Provider store={store}>
+  <App/>
+</Provider>
+);
 
-const logger = creatLogger();
 
-const rootReducer = combineReducers({ searchRobots, requestRobots})
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
-
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.register();
--- src/reducers.js
+++ src/reducers.js
@@ -12,7 +12,7 @@ const intialStateSearch = {
 
 export const searchRobots = (state=intialStateSearch, action={}) => {
     switch(action.type) {
-        case: CHANGE_SEARCH_FIELD:
+        case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, {searchField:action.payload});
         default:
             return state;
