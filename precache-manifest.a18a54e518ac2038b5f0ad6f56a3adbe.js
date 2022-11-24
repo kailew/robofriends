868f913d7d642511602f5a38f3d7ad900a79a241
Kai Lew <79333361+kailew@users.noreply.github.com>
2022-11-23 18:15:47 -0800

    Updates

---
+++ precache-manifest.a18a54e518ac2038b5f0ad6f56a3adbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "656b9590e3fddb8e36cf",
+    "url": "/robofriends/static/css/main.ffa39b43.chunk.css"
+  },
+  {
+    "revision": "656b9590e3fddb8e36cf",
+    "url": "/robofriends/static/js/main.656b9590.chunk.js"
+  },
+  {
+    "revision": "14a04e660fdfdfd4a9a5",
+    "url": "/robofriends/static/css/1.5d702391.chunk.css"
+  },
+  {
+    "revision": "14a04e660fdfdfd4a9a5",
+    "url": "/robofriends/static/js/1.14a04e66.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1be79f0be06f691483f1dc68e0d9f415",
+    "url": "/robofriends/index.html"
+  }
+];
